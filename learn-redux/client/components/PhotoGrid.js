420e8c3ac3f3b9b72733f6d6fd1f1dca522a9bad
Raghav Ramesh <raghavramesh94@gmail.com>
2016-10-08 20:19:35 +0800

    Added reducers (Completed #7)

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const PhotoGrid = React.createClass({
+  render: function() {
+    return (
+      <div className="photo-grid">I'm the photo grid.</div>
+    );
+  }
+});
+
+export default PhotoGrid;
---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const Single = React.createClass({
+  render: function() {
+    return (
+      <div className="single-photo">I'm single</div>
+    );
+  }
+});
+
+export default Single;
