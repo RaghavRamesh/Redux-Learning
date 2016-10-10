1b20a49a29dad491a22a44766ccd7e440a182ce0
Raghav Ramesh <raghavramesh94@gmail.com>
2016-10-10 22:38:28 +0800

    Added UI for comments. (Completed #14)

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { render } from 'react-dom';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render: function() {
+    const { postId } = this.props.params;
+
+    // index of the post
+    const i = this.props.posts.findIndex((post) => post.code === postId);
+
+    // get us the post
+    const post = this.props.posts[i];
+
+    const postComments = this.props.comments[postId] || [];
+
     return (
-      <div className="single-photo">I'm single</div>
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments}/>
+      </div>
     );
   }
 });
